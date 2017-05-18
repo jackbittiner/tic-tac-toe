34b5c7125d80e72c9ccc8d9039eb0684cd3a53d6
Jack Bittiner <jackbittiner@hotmail.com>
2017-05-18 11:01:20 +0100

    Game initializes with two players, X & O

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,14 @@
 require_relative 'board'
+require_relative 'player'
 
 class Game
 
-  attr_reader :board
+  attr_reader :board, :player_1, :player_2
 
   def initialize
     @board = Board.new
+    @player_1 = Player.new('X')
+    @player_2 = Player.new('O')
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,7 +7,14 @@ describe Game do
   describe 'initialize' do
     it 'is initialized with a board' do
       expect(game.board.class).to eq Board
-
     end
+
+    it 'initializes with a player_1 with an X piece' do
+      expect(game.player_1.piece).to eq 'X'
+    end
+    it 'initializes with a player_2 with an O piece' do
+      expect(game.player_2.piece).to eq 'O'
+    end
+
   end
 end
